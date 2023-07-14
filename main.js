0d3a22a1bcf63a0b55e6df2215fdc70f4a3efd82
USER <user@USERnoMacBook-Air.local>
2023-07-14 16:40:31 +0900

    upload final code

---
+++ main.js
@@ -0,0 +1,5 @@
+const pi = 3.14159
+const radius = 3.0
+const area= pi * radius * radius
+const div_text1 =  document.getElementById('text1')
+div_text1.innerHTML = "半径" + String(radius) + 'の円の面積は' +  String(area)
